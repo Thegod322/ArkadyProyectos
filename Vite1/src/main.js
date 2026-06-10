bf103eb26d79a9716002eff3c3d6a8dd4393284f
Thegod322 <carolinadiaz0812@gmail.com>
2026-06-10 13:57:26 +0200

    Añadir sección personalizada con botones de selección y estilos; actualizar archivos SCSS y JavaScript para integrar nueva funcionalidad.

--- Vite1/src/main.js
+++ Vite1/src/main.js
@@ -4,57 +4,53 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-<section id="center">
-  <div class="hero">
-    <img src="${heroImg}" class="base" width="170" height="179">
-    <img src="${javascriptLogo}" class="framework" alt="JavaScript logo"/>
-    <img src="${viteLogo}" class="vite" alt="Vite logo" />
-  </div>
-  <div>
-    <h1>Get started</h1>
-    <p>Edit <code>src/main.js</code> and save to test <code>HMR</code></p>
-  </div>
-  <button id="counter" type="button" class="counter"></button>
-</section>
+const image = document.querySelector('#art-img')
+const cultureButton = document.querySelector('#culture')
+const servicesButton = document.querySelector('#services')
+const artText = document.querySelector('#art-text')
+const descText = document.querySelector('#desc')
+const artData = {
+  services: {
+    imgsrc: "https://www.dummyimage.com/600x400/000/fff",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, consequuntur rerum iure, pariatur, eaque eligendi quasi excepturi veritatis libero in deserunt maxime accusantium eos molestiae distinctio beatae nemo fugit alias.",
+    headline: "FULL<br> TOURS,<br> NO<br> EXCUSES."
+  },
+  culture: {
+    imgsrc: "https://www.dummyimage.com/600x400/f70df7/0011ff",
+    desc: "DOREM ipsum dolor sit amet consectetur adipisicing elit. Iusto, consequuntur rerum iure, pariatur, eaque eligendi quasi excepturi veritatis libero in deserunt maxime accusantium eos molestiae distinctio beatae nemo fugit alias.",
+    headline: "CREW<br> MATTERS<br> NO<br> EXCUSES."
+  }
+}
 
-<div class="ticks"></div>
+cultureButton.onclick = () => {
+  image.src = artData.culture.imgsrc;
+  artText.innerHTML = artData.culture.headline;
+  descText.innerHTML = artData.culture.desc;
+  toggleActive(cultureButton);
+  toggleTextSize(cultureButton);
+}
+servicesButton.onclick = () => {
+  image.src = artData.services.imgsrc;
+  artText.innerHTML = artData.services.headline;
+  descText.innerHTML = artData.services.desc;
+  toggleActive(servicesButton);
+  toggleTextSize(servicesButton);
+}
 
-<section id="next-steps">
-  <div id="docs">
-    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#documentation-icon"></use></svg>
-    <h2>Documentation</h2>
-    <p>Your questions, answered</p>
-    <ul>
-      <li>
-        <a href="https://vite.dev/" target="_blank">
-          <img class="logo" src="${viteLogo}" alt="" />
-          Explore Vite
-        </a>
-      </li>
-      <li>
-        <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-          <img class="button-icon" src="${javascriptLogo}" alt="">
-          Learn more
-        </a>
-      </li>
-    </ul>
-  </div>
-  <div id="social">
-    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#social-icon"></use></svg>
-    <h2>Connect with us</h2>
-    <p>Join the Vite community</p>
-    <ul>
-      <li><a href="https://github.com/vitejs/vite" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#github-icon"></use></svg>GitHub</a></li>
-      <li><a href="https://chat.vite.dev/" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#discord-icon"></use></svg>Discord</a></li>
-      <li><a href="https://x.com/vite_js" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#x-icon"></use></svg>X.com</a></li>
-      <li><a href="https://bsky.app/profile/vite.dev" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#bluesky-icon"></use></svg>Bluesky</a></li>
-    </ul>
-  </div>
-</section>
+function toggleActive(button) {
+  button.classList.add('active');
+  if (button.id === 'culture') {
+    servicesButton.classList.remove('active');
+  } else {
+    cultureButton.classList.remove('active');
+  }
+}
 
-<div class="ticks"></div>
-<section id="spacer"></section>
-`
-
-setupCounter(document.querySelector('#counter'))
+function toggleTextSize(button){
+  button.classList.add('large-text');
+  if(button.id === 'culture'){
+    servicesButton.classList.remove('large-text');
+  } else {
+    cultureButton.classList.remove('large-text');
+  }
+}
