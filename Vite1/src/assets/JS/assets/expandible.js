81f238c6dd594e7cb2066f3db230e3960e2268f2
Thegod322 <carolinadiaz0812@gmail.com>
2026-06-17 13:04:56 +0200

    Añadir múltiples artículos expandibles y mejorar estilos responsivos en SCSS

--- Vite1/src/assets/JS/assets/expandible.js
+++ Vite1/src/assets/JS/assets/expandible.js
@@ -4,11 +4,25 @@ const expandButton = document.querySelector('#expand')
 const expandible = document.querySelector('#expandible')
 const desplegable = document.querySelector('#desplegable')
 
-expandButton.onclick = () => {
-    if(desplegable.classList.contains('expanded')){
-        desplegable.classList.remove('expanded')
-        return
-    }
-    desplegable.classList.add('expanded')
-}
+//expandButton.onclick = () => {
+ //   if(desplegable.classList.contains('expanded')){
+  //      desplegable.classList.remove('expanded')
+  ///      return
+   // }
+  //  desplegable.classList.add('expanded')
+//}//
 
+const expandibles = document.querySelectorAll('.expand')
+expandibles.forEach((boton)=>{
+    boton.addEventListener("click",(e)=>{
+        const article = e.currentTarget.closest('.article')
+        const desplegable = article.querySelector('.desplegable')
+
+        expandibles.forEach((boton)=>{
+            boton.style.backgroundColor="black"
+            boton.style.color="white"
+            boton.style.border = "1px solid transparent"
+        })
+        desplegable.classList.toggle('expanded')
+    })
+})
