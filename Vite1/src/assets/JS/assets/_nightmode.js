600375584dfa8823d467949a611f54f70d0e2401
Thegod322 <carolinadiaz0812@gmail.com>
2026-06-25 14:03:37 +0200

    nightmod

--- Vite1/src/assets/JS/assets/_nightmode.js
+++ Vite1/src/assets/JS/assets/_nightmode.js
@@ -1,4 +1,5 @@
 export default function nightButton(){
+    
     const $btnDarkMode = document.createElement("button");
     $btnDarkMode.className = "night-button";
     $btnDarkMode.innerHTML = `
@@ -6,12 +7,18 @@ export default function nightButton(){
     `;
     //const $btnDarkMode = document.querySelector(".nav__container--darkModeButton");
     $btnDarkMode.addEventListener( "click" , (e)=> {
+        const root = document.documentElement;
         const $icon = $btnDarkMode.querySelector(".fa-solid");
         if($icon.classList.contains("fa-moon")){
             console.log("pizdec")
 
             $icon.classList.remove("fa-moon");
             $icon.classList.add("fa-sun");
+            // Seleccionamos el :root
+
+            // Cambiamos el valor de la variable
+            root.style.setProperty('--color00', '#000000');
+            root.style.setProperty('--primary', 'blue');
         }
         else{
             console.log("zalupkaaa")
