fafa223c6dd65813c77231809dd60066e7fa26b7
Thegod322 <carolinadiaz0812@gmail.com>
2026-07-10 13:54:57 +0200

    465465

--- PHP/index.php
+++ PHP/index.php
@@ -50,7 +50,7 @@ if(isset($_GET['dinero']) && is_numeric($_GET['dinero'])){
             padding: 10px;
             border: solid rgb(208, 252, 255);
             display : flex;
-            justify-self: center;
+            justify-self: flex-start;
             font-size:3rem;
             font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
         }
@@ -60,6 +60,35 @@ if(isset($_GET['dinero']) && is_numeric($_GET['dinero'])){
         }
         a{
             text-decoration: none;
+        }
+        .table-element{
+            display: flex;
+            gap:2rem;
+            border: 1px solid rgba(0, 0, 0, 0.65);
+            justify-content: space-between;
+            align-items: center;
+            padding: 1rem;
+        }
+        .hidden-text{
+            opacity:0;
+            animation: aparecer 0.3s forwards;
+            font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+            font-size:1.5rem;
+            margin:2rem;
+
+
+
+        }
+        .selected{
+            background-color: rgba(197, 255, 170, 0.22);
+        }
+        @keyframes aparecer {
+            from {
+                opacity: 0;
+            }
+            to {
+                opacity: 1;
+            }
         }
     </style>
 </head>
@@ -100,18 +129,77 @@ if(isset($_GET['dinero']) && is_numeric($_GET['dinero'])){
     }
     echo<<<HTML
         <h1>Aqui vamos a mostrar ciudade del array - Ciudades</h1>
+
     HTML;
     $ciudades=["Madrid","Barcelona","Malaga","Moscu","Beijin"];
     foreach($ciudades as $ciudad){
         echo $ciudad."<br>";
     }
 
-        $persona=[
-        "nombre"=>"Gvozdik",
-        "edad"=>"gen-z",
-        "ciudad"=>"Donosti"
+        $personas=
+        [
+            [
+            "nombre"=>"Gvozdik",
+            "direccion" => "Olatz Ederra Kalea, 14, 3º B",
+            "edad"=>"gen-z",
+            "ciudad"=>"Donosti"
+            ],[
+            "nombre"=>"Noelia",
+            "direccion" => "Hondarribi Berria Etorbidea, 5, Bajo C",
+            "edad"=>"boomer",
+            "ciudad"=>"Donosti"
+            ],[
+            "nombre"=>"Erika",
+            "direccion" => "Akelarre Txiki Plaza, 2, 4º Ezkerra",
+            "edad"=>"boomer",
+            "ciudad"=>"Donosti"
+            ],[
+            "nombre"=>"Fabian",
+            "direccion" => "Txalaparta Bidea, 42, 1º D",
+            "edad"=>"boomer",
+            "ciudad"=>"Donosti"
+            ],[
+            "nombre"=>"Niko",
+            "direccion" => "Itxaso Urdin Pasealekua, 8, Ático",
+            "edad"=>"gen-z",
+            "ciudad"=>"Donosti"
+            ],[
+            "nombre"=>"Mauricio",
+            "direccion" => "Mendi Berdearen Galtzada, 105, 2º A",
+            "edad"=>"gen-z",
+            "ciudad"=>"Donosti"
+            ],[
+            "nombre"=>"Ekaterina",
+            "direccion" => "Izargorri Kalea, 33, Entreplanta",
+            "edad"=>"gen-z",
+            "ciudad"=>"Donosti"
+            ],
         ];
-        echo $persona["nombre"]."<br>";
+        $i = 0;
+            foreach($personas as $pers => $valor){
+                $i++;
+                if($_GET['nombre'] == $valor["nombre"]){
+                    
+                    echo <<<HTML
+                    <div class="table-element selected" id="{$i}">
+                        <a href="?nombre={$valor["nombre"]}#{$i}" class ="boton">{$valor["nombre"]}</a>
+                        <p class = "hidden-text">{$valor["direccion"]}</p>
+                        <p class = "hidden-text">{$valor["edad"]}</p>
+                        <p class = "hidden-text">{$valor["ciudad"]}</p>
+                    </div>
+                    HTML ;
+                }
+                else{
+                    echo <<<HTML
+                    <div class="table-element">
+                    <a href="?nombre={$valor["nombre"]}#{$i}" class ="boton">{$valor["nombre"]}</a>
+                    </div>
+                HTML ;
+                }
+                //echo "persona ".$pers."  | valor : ".$valor["nombre"]."<br>";
+            }
+       
+
     ?>
 </body>
 </html>
